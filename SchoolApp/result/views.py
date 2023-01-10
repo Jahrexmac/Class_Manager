a5a200a811c39a25a5d95031cdf14cdc633254d3
Rorobi Anthony <Jahrexmac@gmail.com>
2023-01-10 17:40:22 +0100

    features update

--- SchoolApp/result/views.py
+++ SchoolApp/result/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.views import View
 from .models import Classroom, Student, Subject
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormMixin
+from django.urls import reverse_lazy, reverse
+from django import forms
 
 # Create your views here.
 class Home(View):
@@ -27,7 +28,63 @@ class StudentListView(View):
             return render(request, 'students.html', {'class': std_class, 'class_pk': class_pk})
         else:
             return render(request, 'students.html', {'students': std, 'class': std_class, 'pk': pk,})
+    def post(self,request,pk):
 
+        if 'save' in str(request.body):
+            form = StudentNewForm(request.POST) #to save subject form
+            if form.is_valid():
+                form.save()
+
+            std = Student.objects.filter(student_class=pk)
+            try:
+                std_class = std[0].student_class
+            except IndexError:
+                std = Classroom.objects.filter(pk = pk)
+                std_class = std[0].name
+                class_pk = std[0].pk
+                return render(request, 'students.html', {'class': std_class, 'class_pk': class_pk})
+            else:
+                return render(request, 'students.html', {'students': std, 'class': std_class, 'pk': pk,})
+
+        if 'remove' in str(request.body):
+            student = Student.objects.filter(pk = pk)
+
+            classroom = Classroom.objects.filter(name = student[0].student_class)
+
+            class_pk = classroom[0].pk
+            std = Student.objects.filter(student_class=class_pk)
+
+            Student.objects.filter(pk = pk ).delete() # deletes the student
+            try:
+                std_class = std[0].student_class
+            except IndexError:
+                std = Classroom.objects.filter(pk = pk)
+                std_class = std[0].name
+                class_pk = std[0].pk
+                return render(request, 'students.html', {'class': std_class, 'class_pk': class_pk})
+            else:
+                return render(request, 'students.html', {'students': std, 'class': std_class, 'pk': pk,})
+      
+        if 'update' in str(request.body):
+            print('yes')
+            classroom = Classroom.objects.get(pk=pk)
+            classroom.form_teacher = request.POST['form_teacher']
+            classroom.name = request.POST['name']
+            classroom.save()
+
+            std = Student.objects.filter(student_class=pk)
+
+            Student.objects.filter(pk = pk ).delete() # deletes the student
+            try:
+                std_class = std[0].student_class
+            except IndexError:
+                std = Classroom.objects.filter(pk = pk)
+                std_class = std[0].name
+                class_pk = std[0].pk
+                return render(request, 'students.html', {'class': std_class, 'class_pk': class_pk})
+            else:
+                return render(request, 'students.html', {'students': std, 'class': std_class, 'pk': pk,})
+      
 class ClassCreateView(CreateView):
     model = Classroom
     template_name = "classroomnew.html"
@@ -46,7 +103,9 @@ class ClassUpdate(UpdateView):
 class ClassDelete(DeleteView):
     model = Classroom
     template_name='deleteclassroom.html'
-    success_url = reverse_lazy('class')
+    #success_url = reverse_lazy('student', {'pk':self.object.pk})
+    def get_success_url(self):
+        return reverse('class')
 
 class StudentDetails(View):
     def get(self, request, pk):
@@ -64,6 +123,79 @@ class StudentDetails(View):
         pk = students[0].pk
         return render(request, 'studentdetails.html', {'students': students, 'name': std_name, 'pk': pk, 'class_pk': class_pk, 'subjects':subjects, 'totals': totals})
     
+    def post(self,request,pk):
+        if 'save' in str(request.body):
+            form = SubjectNewForm(request.POST) #to save subject form
+            if form.is_valid():
+                form.save()
+            #print(pk)
+            students = Student.objects.filter(pk=pk) # from data base
+            subjects = Subject.objects.filter(student=pk) # from data base
+
+            subjects = self.subject_process(subjects) # refine the subjects list of subject object to include total, grade and serial number
+        
+            totals = self.subject_total(subjects)
+            #retrieve the class to get the pk so that the back key functionality would work for empty classes
+            classroom = Classroom.objects.filter(name = students[0].student_class) 
+            class_pk = classroom[0].pk
+
+            std_name = students[0].first_name.title() + ' ' + students[0].last_name.title()
+            pk = students[0].pk
+
+            return render(request, 'studentdetails.html', {'students': students, 'name': std_name, 'pk': pk, 'class_pk': class_pk, 'subjects':subjects, 'totals': totals})
+    
+        if 'update' in str(request.body):
+            subject = Subject.objects.get(pk=pk)
+            subject.name = request.POST['name']
+            subject.first_test = request.POST['first_test']
+            subject.second_test = request.POST['second_test']
+            subject.exam = request.POST['exam']
+            subject.save()
+
+            students = Student.objects.filter(subject=pk) # from data base
+            student_pk = students[0].pk
+            subjects = Subject.objects.filter(student=student_pk) # from data base
+
+            subjects = self.subject_process(subjects) # refine the subjects list of subject object to include total, grade and serial number
+        
+            totals = self.subject_total(subjects)
+            #retrieve the class to get the pk so that the back key functionality would work for empty classes
+            classroom = Classroom.objects.filter(name = students[0].student_class) 
+            class_pk = classroom[0].pk
+
+            std_name = students[0].first_name.title() + ' ' + students[0].last_name.title()
+            pk = students[0].pk
+
+            return render(request, 'studentdetails.html', {'students': students, 'name': std_name, 'pk': pk, 'class_pk': class_pk, 'subjects':subjects, 'totals': totals})
+        if 'changes' in str(request.body):
+            
+            student = Student.objects.get(pk=pk)
+            student.first_name = request.POST['first_name']
+            student.last_name = request.POST['last_name']
+            student.middle_name = request.POST['middle_name']
+            student.parent_name =request.POST['parent_name']
+            student.parent_phone_number = request.POST['parent_phone_number']
+            student.house_address = request.POST['house_address']
+            student.religion = request.POST['religion']
+            student.save()
+
+            students = Student.objects.filter(pk=pk) # from data base
+            subjects = Subject.objects.filter(student=pk) # from data base
+
+            subjects = self.subject_process(subjects) # refine the subjects list of subject object to include total, grade and serial number
+        
+            totals = self.subject_total(subjects)
+            #retrieve the class to get the pk so that the back key functionality would work for empty classes
+            classroom = Classroom.objects.filter(name = students[0].student_class) 
+            class_pk = classroom[0].pk
+
+            std_name = students[0].first_name.title() + ' ' + students[0].last_name.title()
+            pk = students[0].pk
+
+            return render(request, 'studentdetails.html', {'students': students, 'name': std_name, 'pk': pk, 'class_pk': class_pk, 'subjects':subjects, 'totals': totals})
+      
+
+
     def subject_process (self, subjects):
         """
             this function adds the serial number, total and grade to the subject object
@@ -94,6 +226,7 @@ class StudentDetails(View):
             average = grand_total / counter
         else:
             average = 0.00
+    
         average = f"{average:.2f}"
 
         if float(average) >= 60:
@@ -120,7 +253,17 @@ class StudentDetails(View):
         else:
             return "A"
         
+class SubjectNewForm(forms.ModelForm):
+    class Meta:
+        model= Subject
+        fields = ["student","name", "first_test", "second_test", "exam"]
 
+class StudentNewForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ["student_class","first_name","last_name","middle_name","parent_name","parent_phone_number","house_address","religion"]
+
+        
 class StudentUpdate(UpdateView):
     model = Student
     template_name = 'updatestudent.html'
